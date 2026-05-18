e65eee878f63a84d6d29a47faced4acdf018fcdb
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-05-18 04:29:05 -0400

    worked on login page

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       data-theme="light"
       className={`${josefin.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-screen flex flex-col">
         <Navbar></Navbar>
         {children}
         <Footer></Footer>
