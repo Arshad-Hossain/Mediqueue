039c1a1bb5a62b95e29c498f42a2a7476cb88a8c
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-05-19 00:48:52 -0400

    setting up better auth & worked on authentication part

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <h2 className="text-2xl text-center">Please wait, data is loading...</h2>
+    </div>
+  );
+};
+
+export default loading;
